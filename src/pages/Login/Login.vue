2ad641f7fb812b5f2ce0714d29430d76e7013dd3
sy5415 <50395945+sy5415@users.noreply.github.com>
2021-09-30 10:19:34 +0800

    Update Login.vue

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,13 +4,13 @@
         <div class="login_header">
           <h2 class="login_logo">硅谷外卖</h2>
           <div class="login_header_title">
-            <a href="javascript:;" :class="{on:isShowPwd}" @click="isShowPwd=true">短信登录</a>
-            <a href="javascript:;" :class="{on:!isShowPwd}" @click="isShowPwd=false" >密码登录</a>
+            <a href="javascript:;" :class="{on:isShowMsg}" @click="isShowMsg=true">短信登录</a>
+            <a href="javascript:;" :class="{on:!isShowMsg}" @click="isShowMsg=false" >密码登录</a>
           </div>
         </div>
         <div class="login_content">
           <form>
-            <div :class="{on:isShowPwd}">
+            <div :class="{on:isShowMsg}">
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
                 <button :disabled="!isRightPhone" class="get_verification" :class="{right_phone_number:isRightPhone}" @click.prevent="sure">获取验证码</button>
@@ -23,16 +23,16 @@
                 <a href="javascript:;">《用户服务协议》</a>
               </section>
             </div>
-            <div :class="{on:!isShowPwd}">
+            <div :class="{on:!isShowMsg}">
               <section>
                 <section class="login_message">
-                  <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名" >
+                  <input type="tel" maxlength="11"  placeholder="手机/邮箱/用户名" >
                 </section>
                 <section class="login_verification">
-                  <input type="tel" maxlength="8" placeholder="密码">
-                  <div class="switch_button off">
-                    <div class="switch_circle"></div>
-                    <span class="switch_text">...</span>
+                  <input :type="isShowPwd ? 'text' : 'password'" maxlength="8" placeholder="密码">
+                  <div class="switch_button off" :class="isShowPwd?'on':'off' " @click="isShowPwd=!isShowPwd">
+                    <div class="switch_circle" :class="{right:isShowPwd}"></div>
+                    <span class="switch_text">  </span>
                   </div>
                 </section>
                 <section class="login_message">
@@ -57,8 +57,9 @@
 export default {
   data(){
     return{
-      isShowPwd:true,
+      isShowMsg:true,
       phone:'',
+      isShowPwd:true
     }
   },
   methods:{
@@ -176,6 +177,8 @@ export default {
                     background #fff
                     box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                     transition transform .3s
+                    &.right
+                      transform translateX(27px)
               .login_hint
                 margin-top 12px
                 color #999
